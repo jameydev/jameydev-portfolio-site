91d8a54a8e452858772e0ef3bda7107e03ccab34
Jamey Bryce <jybryce4@gmail.com>
2023-10-09 18:28:51 -0500

    explicit mime-type setting for slider.js

--- index.js
+++ index.js
@@ -6,28 +6,12 @@
 // Date Created: 10/06/2023
 
 // Import dependencies
-// const Swiper = import('swiper'); maybe not...
 const express = require('express');
 const colors = require('colors');
 const morgan = require('morgan');
 const path = require('path');
 const exphbs = require('express-handlebars');
-
-// const swiper = new Swiper('.swiper-container', {
-//     direction: 'horizontal',
-//     loop: true,
-//     autoplay: {
-//         delay: 5000,
-//     },
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-// });
+const mime = require('mime-types');
 
 // Load dotenv variables
 require('dotenv').config();
@@ -46,6 +30,19 @@ app.set('view engine', 'handlebars');
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the MIME type explicitly for slider.js
+app.get('/js/slider.js', (req, res) => {
+    const filePath = path.join(
+        __dirname, 
+        'public', 
+        'js', 
+        'slider.js'
+    );
+    const mimeType = mime.lookup(filePath);
+    res.set('Content-Type', mimeType);
+    res.sendFile(filePath);
+});
 
 // Routes
 app.use(require('./routes/home'));
