5a13aa12b4fdff47881d59c5760a76f313e34a8c
Jamey Bryce <jybryce4@gmail.com>
2023-10-09 21:14:52 -0500

    added top-of-file comment

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,8 @@
+// File: homeController.js
+// Description: 
+//  Renders the home page
+// Author: Jamey Bryce
+// Date Created: 10/06/2023
 const getHome = (req, res) => {
     res.render('home', {
         title: 'JameyDev - FullStack Developer - Home Page',
