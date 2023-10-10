4e7382497246a3dd1ed90a8ae1fc7aa9c200ef9d
Jamey Bryce <jybryce4@gmail.com>
2023-10-09 21:07:33 -0500

    added top-of-file comment to slider.js

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,3 +1,11 @@
+// File: slider.js
+// Description: 
+//	Sets up the slider
+// 	on the home page
+// 	Uses slick.js
+// Author: Jamey Bryce
+// Date Created: 10/09/2023
+
 $(document).ready(() => {
 	$('.slider').slick({
 		autoplay: true,
